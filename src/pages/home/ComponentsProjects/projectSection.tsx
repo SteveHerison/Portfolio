801483d2cf1517d4ec6eca1fd52027c9d444433a
steveherison <steveherisondev@gmail.com>
2025-01-21 01:54:52 -0300

    Finaliznado o projeto-Te

--- src/pages/home/ComponentsProjects/projectSection.tsx
+++ src/pages/home/ComponentsProjects/projectSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import Gard from "../../../../public/commerce-business-and-finance-svgrepo-com.svg";
+import Gard from "/commerce-business-and-finance-svgrepo-com.svg";
 import { Link } from "@/components/Link";
 import { ArrowBigRightDash } from "lucide-react";
 import { Project } from "@/types/projects";
